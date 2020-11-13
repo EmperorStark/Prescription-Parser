1ce8a254863250cfb3042da66defec6f477d7128
Tony Xi <emperorstark1@gmail.com>
2020-11-12 22:52:57 -0500

    stuff

--- prescription-parser-client/src/app/Features/sig-input/sig-input.component.ts
+++ prescription-parser-client/src/app/Features/sig-input/sig-input.component.ts
@@ -6,6 +6,26 @@ import { SigResponse } from '../Models/SigResponse';
 import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
 @Component({
   selector: 'app-sig-input',
   templateUrl: './sig-input.component.html',
@@ -19,6 +39,8 @@ export class SigInputComponent implements OnInit {
   currentDate: Date;
   drugTimes: any[] = [];
   alert = 0;
+
+  dataSource = ELEMENT_DATA;
 
   maxYear = new Date().getFullYear() + 10; // oldest recorded person was 122 so only let calendar go back 125 years
   maxDate = new Date(this.maxYear, 0, 1); // create date based on the min year
